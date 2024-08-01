c50aa9d344fc1eaf23d4e251b5e96481e75b5c0f
siddharth1703tnr <bca2022siddharth1703@tnraocollege.org>
2024-08-01 12:01:26 +0530

    Create create_class.py

---
+++ create_class.py
@@ -0,0 +1,24 @@
+# assign-3 Program-1
+# 1.Write a program to create class name Employee and display employee name,Date of join ,designation,salary with the help of object.
+
+class Employee:
+    name = "Ram"
+    doj = "14-Aug-2023"
+    deg = "SDE-2"
+    salary = 70000
+    
+emp1 = Employee
+print(emp1.name)
+print(emp1.doj)
+print(emp1.deg)
+print(emp1.salary)
+    
+
+
+# OUTPUT =====>
+# Ram
+# 14-Aug-2023
+# SDE-2
+# 70000
+
+# === Code Execution Successful ===
