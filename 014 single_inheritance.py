3b7ece72a425e30926cc6cff968f742b8d5a8f59
siddharth1703tnr <bca2022siddharth1703@tnraocollege.org>
2024-08-01 12:08:38 +0530

    Create 014 single_inheritance.py

---
+++ 014 single_inheritance.py
@@ -0,0 +1,5 @@
+# assign-3 Program-2
+# 2. Write a program to perform single inheritance create main class company(name,city,mobile no)sub class employee(emp_name,designation,salary).
+
+
+# OUTPUT =====>
